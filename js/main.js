626fb115277f999e6c48ddfd93841b20486a3b5d
Buu90 <55487031+Buu90@users.noreply.github.com>
2020-04-06 17:46:13 +0300

    Add files via upload
    fix

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
 "use strict";
 
-function words() {
-  var k = 0;
-  var stringsearch = ["а", "о", "и", "е", "ё", "э", "ы", "у", "ю", "я"];
-  var f = prompt("Введите строку:");
-  for (var i = 0; i < f.length; i++)
-      for (var j = 0; j < stringsearch.length; j++)
-          if (f[i] === stringsearch[j]) {
-              ++k;
-              break;
-          }
-  return k ? k : "Нет совпадений";
+var q = prompt('Введите строку', '').toLowerCase();
+ 
+function vowels(str) {
+    let v = "аоиеёэыуюя";
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (v.includes(char))
+            ++count;
+    }
+    return count;
 }
-
-console.log(words());
+console.log(vowels(q));
